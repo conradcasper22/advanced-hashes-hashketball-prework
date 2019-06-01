0980191c01dadd0145d1e47a350062792dd15af6
Conrad Casper <conradcasper@icloud.com>
2019-06-01 21:21:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -222,6 +222,7 @@ def big_shoe_rebounds
   hash.each do |location, team_data|
     team_data[:players].each do |player, stats|
       if stats[:shoe] > shoe_size
+        shoe_size = stats[:shoe]
     end
   end
 end

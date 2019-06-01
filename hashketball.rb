c94515bd0f0899296b92c97df5b0713f4862a69d
Conrad Casper <conradcasper@icloud.com>
2019-06-01 20:24:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ def team_names
   hash.each do |location, team_data|
     team_data.collect do |attribute, data|
       if attribute == :team_name
-        return hash[location][:team_name]
+        return hash[location]
       end
     end
   end

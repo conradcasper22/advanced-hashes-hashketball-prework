27aee74aa07dbf360a27a6d7be1d6dc1ad2908a9
Conrad Casper <conradcasper@icloud.com>
2019-06-01 20:49:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -195,6 +195,8 @@ end
 def player_numbers(team_name)
   find_team(team_name)[:players].each do |player, stats|
     stats.collect do |info, num|
+      if info == :number
+        return num
     
    end
   end

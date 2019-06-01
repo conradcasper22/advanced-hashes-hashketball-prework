210df41f2f2f253f7793f0249163cb1a68035ef8
Conrad Casper <conradcasper@icloud.com>
2019-06-01 20:25:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,7 +183,7 @@ def team_names
   hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if attribute == :team_name
-        
+        names << data
       end
     end
   end
